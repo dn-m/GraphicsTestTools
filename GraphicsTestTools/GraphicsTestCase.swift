5b39295146cf825d9c3dbe6b92931b16588e8eab
James <jbean46@gmail.com>
2017-07-27 16:31:35 +0200

    Make fail silently on iOS

--- GraphicsTestTools/GraphicsTestCase.swift
+++ GraphicsTestTools/GraphicsTestCase.swift
@@ -46,11 +46,14 @@ open class GraphicsTestCase: XCTestCase {
     }
 
     private func shell(_ args: String...) -> Int32 {
-        let task = Process()
-        task.launchPath = "/usr/bin/env"
-        task.arguments = args
-        task.launch()
-        task.waitUntilExit()
-        return task.terminationStatus
+        #if os(OSX)
+            let task = Process()
+            task.launchPath = "/usr/bin/env"
+            task.arguments = args
+            task.launch()
+            task.waitUntilExit()
+            return task.terminationStatus
+        #endif
+        return 0
     }
 }
